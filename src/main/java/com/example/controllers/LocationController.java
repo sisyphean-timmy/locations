b3502d14182de6fc1c67cc818b5623c999484a5b
timmy <>
2022-06-08 11:55:23 +0800

    feat: 依縣市代碼及鄉鎮代碼取得村里 and deprecate 依給定經緯度取得距離最近的氣象觀測站

--- src/main/java/com/example/controllers/LocationController.java
+++ src/main/java/com/example/controllers/LocationController.java
@@ -28,16 +28,41 @@ import net.sf.json.xml.XMLSerializer;
 @RestController
 public class LocationController {
 
+    final private String CWB_KEY = "CWB-6BD4A26E-3011-4BEC-B229-706658ADBBFF";
+
     final private String STATIONS_URL = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/C-B0074-002?Authorization=rdec-key-123-45678-011121314";
-    
-    /**@see https://data.gov.tw/dataset/152915 */
+
+    /**
+     * 氣象站條件( id、name ) 篩選不作用
+     * @see https://e-service.cwb.gov.tw/wdps/obs/state.htm
+     * 預報因子中文對應
+     * @see https://opendata.cwb.gov.tw/opendatadoc/DIV2/A0003-001.pdf
+     */
+    final private String OBS_URL = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001";
+
+    /**
+     * 單點坐標回傳行政區(戶政)
+     * @see https://data.gov.tw/dataset/152915 
+    */
     final private String LATLNG_QUERY_URL = "https://api.nlsc.gov.tw/other/TownVillagePointQuery1/{lng}/{lat}/4326";
     
-    /**@see https://data.gov.tw/dataset/101905 */
+    /**
+     * 代碼服務－縣市清單
+     * @see https://data.gov.tw/dataset/101905 
+     */
     final private String COUNTY_URL = "https://api.nlsc.gov.tw/other/ListCounty";
 
-    /**@see https://data.gov.tw/dataset/102013 */
+    /**
+     * 代碼服務－鄉鎮市區清單(地政)
+     * @see https://data.gov.tw/dataset/102013 
+     */
     final private String TOWNSHIP_URL = "https://api.nlsc.gov.tw/other/ListTown/{code}";
+    
+    /**
+     * 代碼服務－村里清單
+     * @see https://data.gov.tw/dataset/102017
+     */
+    final private String VILLAGE_URL = "https://api.nlsc.gov.tw/other/ListVillage/{ctyCode}/{townCode}";
     
     @Autowired
     private RestTemplate restTemplate;
@@ -130,7 +155,7 @@ public class LocationController {
             XMLSerializer xmlSerializer = new XMLSerializer();
             JSON json = xmlSerializer.read(response.getBody());
             JSONArray jsonArr = JSONArray.fromObject(json);
-    
+            
             ArrayList<Map<String,String>> result = new ArrayList<>();
             for (Object obj : jsonArr) {
                 result.add(Map.of(
@@ -146,8 +171,44 @@ public class LocationController {
         }    
     }
     
+    
+    @GetMapping("/county/{ctyCode}/township/{townCode}")
+    @Operation(summary = "依縣市代碼及鄉鎮代碼取得村里")
+    public ResponseEntity<?> getVillages(
+        @PathVariable("ctyCode") String ctyCode,
+        @PathVariable("townCode") String townCode
+    ){
+        try{
+            ResponseEntity<String> response = restTemplate.exchange(
+                VILLAGE_URL.replace("{ctyCode}", ctyCode).replace("{townCode}", townCode),
+                HttpMethod.GET,
+                null,
+                String.class
+            );
+
+            
+            XMLSerializer xmlSerializer = new XMLSerializer();
+            JSON json = xmlSerializer.read(response.getBody());
+            JSONArray jsonArr = JSONArray.fromObject(json);
+            
+            ArrayList<Map<String,String>> result = new ArrayList<>();
+            for (Object obj : jsonArr) {
+                result.add(Map.of(
+                    "code", JSONObject.fromObject(obj).getString("villageId"),
+                    "value",JSONObject.fromObject(obj).getString("villageName")
+                ));
+            }
+
+            return ResponseEntity.ok(result);
+        }catch(Exception e){
+            // e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
+        }    
+    }
+
+    @Deprecated
     @GetMapping("/weather/station")
-    @Operation(summary = "依給定經緯度取得距離最近的氣象觀測站")
+    @Operation(summary = "依給定經緯度取得距離最近的氣象觀測站 ( CWB API 參數 stationID、stationName 無法篩選 )")
     public ResponseEntity<?> getClosetStation (
         @RequestParam(name = "lng", required = true) String lng,
         @RequestParam(name = "lat", required = true) String lat
@@ -190,6 +251,47 @@ public class LocationController {
             // e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
         }
+    }
+
+    @GetMapping("/weather")
+    @Operation(summary = "依給定經緯度取得距離最近的氣象觀測站的最新觀測資料")
+    public ResponseEntity<?> getObsWeather (
+        @RequestParam(name = "lng", required = true) String lng,
+        @RequestParam(name = "lat", required = true) String lat
+    ){
+        try{
+            String url = UriComponentsBuilder.fromHttpUrl(OBS_URL)
+                .queryParam("Authorization", CWB_KEY)
+                .toUriString();
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                String.class
+            );
+
+            JSONArray locations = JSONObject.fromObject(response.getBody())
+                .getJSONObject("records")
+                .getJSONArray("location");
+
+            ArrayList<double[]> points = new ArrayList<>();
+
+            for (Object location : locations) {
+                double _lng = Double.parseDouble(JSONObject.fromObject(location).get("lon").toString());
+                double _lat = Double.parseDouble(JSONObject.fromObject(location).get("lat").toString());
+                points.add(new double[] {_lat, _lng});
+            }
+            Integer idx = ClosestPoint.nearestPoint(
+                new double[] { Double.parseDouble(lat),Double.parseDouble(lng) }, 
+                points.toArray(new double[points.size()][])
+            );
+
+            return ResponseEntity.ok(locations.get(idx));
+        }catch(Exception e){
+            // e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
+        }    
     }
 
     @GetMapping("/proxy")
